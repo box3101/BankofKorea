e92abf158b02cd0ea784e58abaaa6012ca7467bc
cylee_ispark <tony.at.cp@gmail.com>
2023-08-06 20:05:15 +0900

    중간점검 업로드

--- public/assets/js/custom/ui.js
+++ public/assets/js/custom/ui.js
@@ -4,20 +4,22 @@
   function comBtnActive() {
     const rippleElements = document.querySelectorAll('.com-btn');
 
-    rippleElements.forEach(element => {
-      element.addEventListener('click', e => {
-        const rippleElement = document.createElement('span');
-        rippleElement.classList.add('v-ripple-overlay');
-        element.appendChild(rippleElement);
-        const x = e.clientX - e.target.offsetLeft;
-        const y = e.clientY - e.target.offsetTop;
-        rippleElement.style.left = x + 'px';
-        rippleElement.style.top = y + 'px';
-        setTimeout(() => {
-          rippleElement.remove();
-        }, 500);
+    if (rippleElements) {
+      rippleElements.forEach(element => {
+        element.addEventListener('click', e => {
+          const rippleElement = document.createElement('span');
+          rippleElement.classList.add('v-ripple-overlay');
+          element.appendChild(rippleElement);
+          const x = e.clientX - e.target.offsetLeft;
+          const y = e.clientY - e.target.offsetTop;
+          rippleElement.style.left = x + 'px';
+          rippleElement.style.top = y + 'px';
+          setTimeout(() => {
+            rippleElement.remove();
+          }, 500);
+        });
       });
-    });
+    }
   }
 
   // 네비게이션
@@ -47,17 +49,52 @@
     const $$tabs = document.querySelectorAll(".com-tab-wrap .head li");
     const $$tabContents = document.querySelectorAll(".tab-contents .cnt__body");
 
-    $$tabs.forEach(function (tab, idx) {
-      tab.addEventListener("click", function (e) {
-        e.preventDefault();
-        $$tabs.forEach(innerTab => innerTab.classList.remove("on"));
-        tab.classList.add("on");
+    if ($$tabs) {
+      $$tabs.forEach(function (tab, idx) {
+        tab.querySelector("button").addEventListener("click", function (e) {
+          e.preventDefault();
+          $$tabs.forEach(innerTab => innerTab.classList.remove("on"));
+          tab.classList.add("on");
 
-        $$tabContents.forEach(cnt => cnt.style.display = "none");
-        $$tabContents[idx].style.display = "block";
+          $$tabContents.forEach(cnt => cnt.style.display = "none");
+          $$tabContents[idx].style.display = "block";
+        });
+      });
+    }
+  }
+
+  // 공통 select 커스텀
+  function comSelect() {
+    const $$selects = document.querySelectorAll('.cont-select');
+    const $$btnSet = document.querySelectorAll('.btn-select');
+    const $body = document.querySelector('body');
+
+    $body.addEventListener("click", (e) => {
+      $$btnSet.forEach((el) => {
+        if (el.classList.contains("on") && e.target != el) el.classList.remove("on")
+      })
+    })
+
+    $$selects.forEach((el) => {
+      let btnSel = el.querySelector(".btn-select");
+      let listMember = el.querySelector(".list-member");
+      btnSel.addEventListener("click", () => {
+        $$btnSet.forEach((el) => el.classList.remove("on"))
+        btnSel.classList.toggle("on");
+      });
+      listMember.addEventListener("click", (e) => {
+        if (e.target.classList.contains('list-item')) {
+          let innerTxt = e.target.innerText;
+          btnSel.innerText = innerTxt;
+          btnSel.classList.remove("on");
+        }
       });
     });
   }
+
+
+
+  // btnSel이 on이고 e.target이 listMeber가 아닐땐 
 
   // 공통 팝업
   document.addEventListener('DOMContentLoaded', function () {
@@ -114,39 +151,18 @@
       }
   });
 
-  // 달력
-  function datePicker() {
-    const $$datePickerbtns = document.querySelectorAll(".datepicker-button");
-
-    $$datePickerbtns.forEach((btn, idx) => {
-      const input = document.querySelectorAll('.datepicker-input')[idx];
-
-      const datepicker = flatpickr(input, {
-        dateFormat: "Y.m.d",
-        allowInput: true,
-        locale: 'ko'
-      });
-
-      btn.addEventListener("click", () => {
-        datepicker.open();
-      })
-    });
-  }
-
-  // 공통 로딩후
-  function updateOnEvt(){
+  // 공통 모션
+  function updateOnEvt() {
     document.querySelectorAll('.cnt-wrap').forEach(el => el.classList.add("v-motion"));
     document.querySelectorAll('.tab-contents .tab-item').forEach(el => el.classList.add("v-motion"));
   }
 
   function eventBinding() {
-    if (document.querySelector('.com-btn')) comBtnActive();
-    if (document.querySelector(".com-tab-wrap")) comTab();
-    if (document.querySelector(".com-date-range")) datePicker();
-
-    // 공통
+    comBtnActive();
+    comTab();
     navigator();
     updateOnEvt();
+    comSelect();
   }
 
   function init() {
